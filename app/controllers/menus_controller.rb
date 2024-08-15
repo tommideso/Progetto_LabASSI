cacd2485b259c7d489a17f3363569f3f462e454b
layard <114928369+layard32@users.noreply.github.com>
2024-08-15 18:55:14 +0200

    refactor: update database.yml and start create action
    Co-Authored-By: AndreLanza1 <165406741+AndreLanza1@users.noreply.github.com>
    Co-Authored-By: Elisa1011 <144530560+Elisa1011@users.noreply.github.com>
    Co-Authored-By: ClaudiaCornacchia <155066482+ClaudiaCornacchia@users.noreply.github.com>
    Co-Authored-By: tommideso <58049345+tommideso@users.noreply.github.com>

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -12,19 +12,28 @@ class MenusController < ApplicationController
         @menu = Menu.new
     end
 
-    # def create
-    #     @menu = BlogPost.new(params_blog_post)
-    #     if @post.save
-    #         redirect_to @post
-    #     else
-    #         render :new, status: :unprocessable_entity
-    #     end
-    # end
+    def create
+        @menu = BlogPost.new(params_blog_post)
+        if @post.save
+            redirect_to @post
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
 
     private
+    
     def find_menu
         @menu = Menu.find(params[:id])
         rescue ActiveRecord::RecordNotFound
             redirect_to root_path 
+    end
+
+    def menu_params
+        # non permetto il passaggio del parametro disattivato! (solo per gli admin, per questioni di sicurezza)
+        params.require(:menu).permit(:titolo, :descrizione, :prezzo_persona,
+                                    :min_persone, :max_persone, :tipo_cucina,
+                                    :allergeni, :preferenze_alimentari, :adattabile,
+                                    :extra, :prezzo_extra)
     end
 end 
