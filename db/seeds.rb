b391d877f9813a9b3ce44aaa1715eb3ea848bbbf
AndreLanza1 <andrea@lanzarone.eu>
2024-08-15 19:21:02 +0200

    refactor: rollback
    Co-Authored-By: layard <114928369+layard32@users.noreply.github.com>
    Co-Authored-By: tommideso <58049345+tommideso@users.noreply.github.com>
    Co-Authored-By: Elisa1011 <144530560+Elisa1011@users.noreply.github.com>
    Co-Authored-By: ClaudiaCornacchia <155066482+ClaudiaCornacchia@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,30 +10,57 @@
 
 menu1 = Menu.find_or_initialize_by(titolo: "Menu Vegano")
 menu1.update!(
-  descrizione: "Un menu adatto a chi segue una dieta vegana.",
-  prezzo_persona: 25.50,
-  min_persone: 2,
-  max_persone: 10,
-  tipo_cucina: "Vegano",
-  allergeni: { "glutine" => false, "soia" => true, "noci" => true },
-  preferenze_alimentari: { "vegano" => true, "vegetariano" => true },
-  adattabile: { "senza_glutine" => true, "senza_soia" => false },
-  extra: { "bevande_incluse" => false, "dessert" => true },
-  prezzo_extra: 5,
-  disattivato: false
+    descrizione: "Un menu adatto a chi segue una dieta vegana.",
+    prezzo_persona: 25.50,
+    min_persone: 2,
+    max_persone: 10,
+    tipo_cucina: "Vegano",
+    allergeni: { "glutine" => false, "soia" => true, "noci" => true, "lattosio" => false, "crostacei" => true, "arachidi" => false },
+    preferenze_alimentari: { "vegano" => true, "glutine" => true },
+    adattabile: {
+        "preferenze" => {
+            "vegano" => true,
+            "glutine" => false
+        },
+        "allergeni" => {
+            "glutine" => false, 
+            "soia" => true, 
+            "noci" => true, 
+            "lattosio" => false, 
+            "crostacei" => true, 
+            "arachidi" => false
+        }
+    },
+    extra: { "bevande_incluse" => false, "dessert" => true },
+    prezzo_extra: 5,
+    disattivato: false
 )
 
 menu2 = Menu.find_or_initialize_by(titolo: "Menu Carnivoro")
 menu2.update!(
-  descrizione: "Un menu ricco di piatti a base di carne, ideale per gli amanti della carne.",
-  prezzo_persona: 30.00,
-  min_persone: 3,
-  max_persone: 12,
-  tipo_cucina: "Carnivoro",
-  allergeni: { "glutine" => true, "soia" => false, "noci" => false },
-  preferenze_alimentari: { "carnivoro" => true },
-  adattabile: { "senza_glutine" => false, "senza_soia" => true },
-  extra: { "bevande_incluse" => true, "dessert" => true },
-  prezzo_extra: 10,
-  disattivato: false
+    descrizione: "Un menu ricco di piatti a base di carne, ideale per gli amanti della carne.",
+    prezzo_persona: 30.00,
+    min_persone: 3,
+    max_persone: 12,
+    tipo_cucina: "Carnivoro",
+    allergeni: { "glutine" => false, "soia" => true, "noci" => true, "lattosio" => false, "crostacei" => true, "arachidi" => false },
+    preferenze_alimentari: { "vegano" => true, "glutine" => false },
+    adattabile: {
+        "preferenze" => {
+            "vegano" => true,
+            "glutine" => false
+        },
+        "allergeni" => {
+            "glutine" => false, 
+            "soia" => true, 
+            "noci" => true, 
+            "lattosio" => false, 
+            "crostacei" => true, 
+            "arachidi" => false
+        }
+    },
+    extra: { "bevande_incluse" => true, "dessert" => true },
+    prezzo_extra: 10,
+    disattivato: false
+)
 )
