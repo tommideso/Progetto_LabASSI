e010f481eb361a5922da9e583f83562412fa42bb
layard <114928369+layard32@users.noreply.github.com>
2024-08-16 13:01:33 +0200

    Revert "build: create 3 devise users"
    This reverts commit f0b6a36f055dcc88aee1537eb53268d6a04b1446.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :clients
-  devise_for :chefs
-  devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
