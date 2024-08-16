9ee94d024925bf10ab21d627aaa2831af61f111c
AndreLanza1 <andrea@lanzarone.eu>
2024-08-16 18:39:19 +0200

    refactor: validation for the extra
    Co-Authored-By: ClaudiaCornacchia <155066482+ClaudiaCornacchia@users.noreply.github.com>
    Co-Authored-By: Elisa1011 <144530560+Elisa1011@users.noreply.github.com>
    Co-Authored-By: layard <114928369+layard32@users.noreply.github.com>
    Co-Authored-By: tommideso <58049345+tommideso@users.noreply.github.com>

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -4,11 +4,10 @@ class Menu < ApplicationRecord
 
     # per l'attributo booleano definisco il validates usando incluse per evitare problemi se è false
     validates :disattivato, inclusion: { in: [true, false] }
-    validates :prezzo_extra, presence: true, if: -> { extra.present? }
+    validates :prezzo_extra, presence: true, if: -> { extra_has_selected? }
     validates :extra, presence: true, if: -> { prezzo_extra.present? }
 
-    # validazione personalizzata per fare in modo che max_persone sia maggiore di min_persone
-    validate :max_persone_maggior_di_min_persone
+    validate :prezzo_extra_has_at_least_one_extra, if: -> { prezzo_extra.present? }
 
     # definiamo direttamente nel modello delle liste immutabili per la lista degli allergeni, le preferenze alimentari e il tipo di cucina
     # (così se vogliamo modificarli, li dobbiamo modificare solamente qua)
@@ -18,10 +17,6 @@ class Menu < ApplicationRecord
     TIPI_CUCINA = ["mare", "terra", "cinese", "indiano"].freeze
     EXTRA = ["vino", "mise en place"].freeze
 
-    # NOTA: interpretiamo 'adattabile' come un dizionario con due chiavi ('preferenze' e 'allergeni') e per ognuna di queste chiavi si va
-    # a definire l'adattabilità per ogni possibili valore
-
-
     private
 
     def max_persone_maggior_di_min_persone
@@ -29,5 +24,17 @@ class Menu < ApplicationRecord
         if max_persone.present? && min_persone.present? && max_persone < min_persone
             errors.add(:max_persone, "deve essere maggiore rispetto a min_persone")
         end
+    end
+
+    def prezzo_extra_has_at_least_one_extra
+        # Verifica che almeno uno degli extra sia selezionato
+        selected_extras = extra.select { |key, value| value == "true" }.keys
+        unless (selected_extras & EXTRA).any?
+            errors.add(:extra, "almeno uno degli extra deve essere selezionato")
+        end
+    end
+
+    def extra_has_selected?
+        extra.present? && extra.values.any? { |value| value == "true" }
     end
 end
