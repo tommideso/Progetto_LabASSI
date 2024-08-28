0b1b128ba95310f80ddda534ab4c74243054e2a3
tommi.deso <tommi.deso@gmail.com>
2024-08-28 16:41:50 +0200

    refactor: redirects and params in complete registration
    Co-Authored-By: ClaudiaCornacchia <155066482+ClaudiaCornacchia@users.noreply.github.com>
    Co-Authored-By: Elisa1011 <144530560+Elisa1011@users.noreply.github.com>
    Co-Authored-By: layard <114928369+layard32@users.noreply.github.com>
    Co-Authored-By: AndreLanza1 <165406741+AndreLanza1@users.noreply.github.com>

--- app/controllers/complete_registrations_controller.rb
+++ app/controllers/complete_registrations_controller.rb
@@ -3,16 +3,23 @@ class CompleteRegistrationsController < ApplicationController
   before_action :set_user
 
   def new
-    # TODO controllare il parametro role se è valido, forse scambiare ordine delle condizioni
-    if (params[:role])
-      @user.update(ruolo: params[:role])
-      @user.update_column(:completed, 1)
-      # sfruttiamo la notazione nestata (client e chef vengono considerati 'dentro' user)
-      @chef = @user.chef || @user.build_chef if params[:role] == 'chef'
-      @client = @user.client || @user.build_client if params[:role] == 'client'
-    elsif (@user.completed == 1)
+    if @user.completed == 2 # Profilo completato
+      redirect_to root_path, notice: 'Profilo già completato.'    
+    elsif (@user.completed == 1) # Ruolo selezionato
       @chef = @user.chef || @user.build_chef if @user.chef?
       @client = @user.client || @user.build_client if @user.client?
+      clean_url_from_role_param_and_redirect
+    elsif (params[:role]) # Ruolo cliccato nel form
+        if params[:role] == 'chef' || params[:role] == 'client'
+          @user.update(ruolo: params[:role])
+          @user.update_column(:completed, 1)
+          # sfruttiamo la notazione nestata (client e chef vengono considerati 'dentro' user)
+          @chef = @user.chef || @user.build_chef if params[:role] == 'chef'
+          @client = @user.client || @user.build_client if params[:role] == 'client'
+          clean_url_from_role_param_and_redirect
+        else
+          redirect_to new_complete_registration_path, alert: 'Ruolo non valido.'
+        end
     else
       render :role_selection
     end
@@ -40,5 +47,11 @@ class CompleteRegistrationsController < ApplicationController
     params.require(:user).permit(:nome, :cognome, :ruolo, :completed,
     chef_attributes: [:indirizzo, :telefono, :raggio, :descrizione, :id], 
     client_attributes: [:indirizzo, :telefono, { allergeni: {} }, :id])
+  end
+  def clean_url_from_role_param_and_redirect
+    if params[:role].present? # Se il ruolo è stato selezionato, tolgo il parametro role dall'url per renderlo clean
+      clean_params = request.query_parameters.except(:role)
+      redirect_to new_complete_registration_path(clean_params) and return
+    end
   end
 end
