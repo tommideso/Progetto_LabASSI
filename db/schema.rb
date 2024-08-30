a5537915a761ee4a0af408be444069e56d0f4584
layard <114928369+layard32@users.noreply.github.com>
2024-08-30 21:19:33 +0200

    feat: add room and users view
    Co-Authored-By: AndreLanza1 <165406741+AndreLanza1@users.noreply.github.com>
    Co-Authored-By: ClaudiaCornacchia <155066482+ClaudiaCornacchia@users.noreply.github.com>
    Co-Authored-By: Elisa1011 <144530560+Elisa1011@users.noreply.github.com>
    Co-Authored-By: tommideso <58049345+tommideso@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_30_164623) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_30_190941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -98,6 +98,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_30_164623) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "data_prenotazione"], name: "index_reservations_on_user_id_and_data_prenotazione", unique: true
     t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
