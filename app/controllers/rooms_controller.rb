93b2431060cb0c0b9bcda6cb51f265a081a4ae20
layard <114928369+layard32@users.noreply.github.com>
2024-08-30 22:26:08 +0200

    refactor: add create and show action for room
    Co-Authored-By: AndreLanza1 <165406741+AndreLanza1@users.noreply.github.com>
    Co-Authored-By: ClaudiaCornacchia <155066482+ClaudiaCornacchia@users.noreply.github.com>
    Co-Authored-By: Elisa1011 <144530560+Elisa1011@users.noreply.github.com>
    Co-Authored-By: tommideso <58049345+tommideso@users.noreply.github.com>

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,4 +8,23 @@ class RoomsController < ApplicationController
     @users = User.all_except(current_user)
     render 'index'
   end
+
+  def create
+    # creiamo la stanza dandogli il nome associato alla chiave room dentro params
+    @room = Room.create(name: params['room']['name'])
+  end
+
+  def show
+    # troviamo la stanza con il relativo id
+    @single_room = Room.find(params[:id])
+    @room = Room.new
+
+    # inizializziamo i messaggi e li mostriamo in ordine ascendente
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+
+    # TODO: rimuovere
+    @users = User.all_except(current_user)
+    render 'index'
+  end
 end
