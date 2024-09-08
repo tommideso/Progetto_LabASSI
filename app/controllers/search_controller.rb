33ef9ac23d664f239d1d4e557345012a796962c2
Elisa1011 <elisabenedettucci@gmail.com>
2024-09-08 20:05:37 +0200

    fix: search
    Co-Authored-By: AndreLanza1 <165406741+AndreLanza1@users.noreply.github.com>
    Co-Authored-By: ClaudiaCornacchia <155066482+ClaudiaCornacchia@users.noreply.github.com>
    Co-Authored-By: layard <114928369+layard32@users.noreply.github.com>
    Co-Authored-By: tommideso <58049345+tommideso@users.noreply.github.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,29 @@ class SearchController < ApplicationController
   def index
     @q = Menu.ransack(params[:q])
     @menus = @q.result(distinct: true)
+
+    #filtri personalizzati
+    if params[:numero_persone].present?
+      numero_persone = params[:numero_persone].to_i
+      @menus = @menus.where('min_persone <= ? AND max_persone >= ?', numero_persone, numero_persone)
+    end
+
+    if params[:allergeni].present?
+      allergeni_query = params[:allergeni].map { |allergene| "allergeni ->> '#{allergene}' = 'false'" }.join(' AND ')
+      @menus = @menus.where(allergeni_query)
+    end
+
+    if params[:preferenze].present?
+      preferenze_query = params[:preferenze].map { |preferenza| "preferenze_alimentari ->> '#{preferenza}' = 'true'" }.join(' AND ')
+      @menus = @menus.where(preferenze_query)
+    end
+
+    
+
+
+
   end
+
+  
+
 end
