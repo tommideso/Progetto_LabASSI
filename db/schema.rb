b31e240e4423c11976f0f3e2c88e8996a2dcd5a1
Elisa1011 <elisabenedettucci@gmail.com>
2024-09-08 20:17:19 +0200

    fix: timestamp migration
    Co-Authored-By: AndreLanza1 <165406741+AndreLanza1@users.noreply.github.com>
    Co-Authored-By: ClaudiaCornacchia <155066482+ClaudiaCornacchia@users.noreply.github.com>
    Co-Authored-By: layard <114928369+layard32@users.noreply.github.com>
    Co-Authored-By: tommideso <58049345+tommideso@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_08_162712) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_07_011705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
