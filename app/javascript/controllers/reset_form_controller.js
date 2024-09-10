828181998f4df0785693e2ada9ed1b2ac31d3af3
layard <114928369+layard32@users.noreply.github.com>
2024-09-10 16:03:28 +0200

    refactor: add chat access from admin page
    Co-Authored-By: AndreLanza1 <165406741+AndreLanza1@users.noreply.github.com>
    Co-Authored-By: ClaudiaCornacchia <155066482+ClaudiaCornacchia@users.noreply.github.com>
    Co-Authored-By: Elisa1011 <144530560+Elisa1011@users.noreply.github.com>
    Co-Authored-By: tommideso <58049345+tommideso@users.noreply.github.com>

---
+++ app/javascript/controllers/reset_form_controller.js
@@ -0,0 +1,4 @@
+// import { Controller } from "@hotwired/stimulus";
+
+// // usiamo stimulus per iniettare codice js allo scopo di resettare il form di invio del messaggio nella chat
+// export default class extends Controller
