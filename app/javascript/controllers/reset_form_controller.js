42a3ee98928791cc5a484204e10586b5e60f23b9
layard <114928369+layard32@users.noreply.github.com>
2024-09-10 16:14:12 +0200

    refactor: add new message form reset via stimulus
    Co-Authored-By: AndreLanza1 <165406741+AndreLanza1@users.noreply.github.com>
    Co-Authored-By: ClaudiaCornacchia <155066482+ClaudiaCornacchia@users.noreply.github.com>
    Co-Authored-By: Elisa1011 <144530560+Elisa1011@users.noreply.github.com>
    Co-Authored-By: tommideso <58049345+tommideso@users.noreply.github.com>

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -1,4 +1,12 @@
-// import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus";
 
-// // usiamo stimulus per iniettare codice js allo scopo di resettare il form di invio del messaggio nella chat
-// export default class extends Controller
+// usiamo stimulus per iniettare codice js allo scopo di resettare il form di invio del messaggio nella chat
+export default class extends Controller {
+    reset() {
+        this.element.reset();
+        const chatText = this.element.querySelector('#chat-text');
+        if (chatText) {
+            chatText.style.height = "auto";
+        }
+    }
+}
