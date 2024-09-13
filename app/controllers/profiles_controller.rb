6502236962d8704c37b595fd88d2c44adbff79a7
tommi.deso <tommi.deso@gmail.com>
2024-09-13 18:01:27 +0200

    feat: chef menus page and reservations page admin side style: seach modal
    Co-authored-by: AndreaLanza1 <165406741+AndreLanza1@users.noreply.github.com>
    Co-authored-by: ClaudiaCornacchia <155066482+ClaudiaCornacchia@users.noreply.github.com>
    Co-authored-by: Elisa1011 <elisabenedettucci@gmail.com>
    Co-authored-by: layard <114928369+layard32@users.noreply.github.com>

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-    before_action :authenticate_user!, only: [ :block, :unblock, :index ]
+    before_action :authenticate_user!, only: [ :block, :unblock, :index, :reservations ]
     before_action :check_if_admin, only: [ :block, :unblock, :index ]
 
     # Ho già controllo se l'utente è admin
@@ -54,5 +54,37 @@ class ProfilesController < ApplicationController
         @user.unlock_access!
         flash[:notice] = "Utente sbloccato"
         redirect_to profile_path(@user.id)
+    end
+
+    # Mostro i menu di uno chef
+    def menus
+        @user = User.find(params[:id])
+        if @user.client?
+            flash[:alert] = "Un cliente non può avere menu"
+            redirect_to root_path
+            return
+        end
+        if user_signed_in? && (current_user.id == @user.id || current_user.admin?)
+            @menus = Menu.where(chef: @user.chef).order(created_at: :desc)
+        else
+            # Se l'utente non è loggato o non è l'utente giusto, mostro solo i menu attivi
+            @menus = Menu.where(chef: @user.chef).where(disattivato: false).order(created_at: :desc)
+        end
+        render "menus/index"
+    end
+
+    # Mostro le prenotazioni di un utente
+    def reservations
+        @user = User.find(params[:id])
+        if !current_user.admin? && current_user.id != @user.id
+            flash[:alert] = "Non hai i permessi necessari per vedere questa pagina"
+            redirect_to root_path
+            return
+        end
+        @reservations = (@user.chef? ? @user.chef.reservations : @user.client.reservations).order(created_at: :desc)
+        @next_reservations = @reservations.where("data_prenotazione >= ?", Date.today).where(stato: [ :attesa_pagamento, :confermata ]).order(data_prenotazione: :asc)
+        @cancelled_reservations = @reservations.where(stato: [ :cancellata ]).order(data_prenotazione: :desc)
+        @completed_reservations = @reservations.where(stato: [ :completata, :rimborsata ]).order(data_prenotazione: :desc)
+        render "reservations/index"
     end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,29 +3,25 @@ class SearchController < ApplicationController
     @q = Menu.ransack(params[:q])
     @menus = @q.result(distinct: true)
 
-
-    #filtri personalizzati
+    puts "PARAMS: #{params}"
+    # filtri personalizzati
     if params[:numero_persone].present?
-      numero_persone = params[:numero_persone].to_i
-      @menus = @menus.where('min_persone <= ? AND max_persone >= ?', numero_persone, numero_persone)
+      numero_persone = params[:numero_persone]
+      @menus = @menus.where("min_persone <= ? AND max_persone >= ?", numero_persone, numero_persone)
     end
 
     if params[:allergeni].present?
-      allergeni_query = params[:allergeni].map { |allergene| "allergeni ->> '#{allergene}' = 'false'" }.join(' AND ')
+      allergeni_query = params[:allergeni].map { |allergene| "allergeni ->> '#{allergene}' = 'false'" }.join(" AND ")
       @menus = @menus.where(allergeni_query)
     end
 
     if params[:preferenze].present?
-      preferenze_query = params[:preferenze].map { |preferenza| "preferenze_alimentari ->> '#{preferenza}' = 'true'" }.join(' AND ')
+      preferenze_query = params[:preferenze].map { |preferenza| "preferenze_alimentari ->> '#{preferenza}' = 'true'" }.join(" AND ")
       @menus = @menus.where(preferenze_query)
     end
 
     if params[:search_date].present?
       @menus = @menus.disponibili_per_data(params[:search_date])
     end
-
   end
-
-  
-
 end
